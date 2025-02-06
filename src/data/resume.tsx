25b1e5920af011c632b2546e751e39185a9e48f2
joand <joandyalejogarcia@outlook.com>
2025-02-06 01:31:19 -0500

    test

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -81,7 +81,7 @@ export const DATA = {
     {
       school: "Bloomsburg University Of Pennsylvania",
       href: "https://www.commonwealthu.edu/",
-      degree: "Bachelor's Degree of Computer Science (BCS)",
+      degree: "Bachelor's Degree of Computer Science",
       logoUrl: "/blooms.png",
       start: "2022",
       end: "2026",
@@ -95,130 +95,130 @@ export const DATA = {
       end: "2020",
     },
   ],
-  projects: [
-    {
-      title: "Chat Collect",
-      href: "https://chatcollect.com",
-      dates: "Jan 2024 - Feb 2024",
-      active: true,
-      description:
-        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Stripe",
-        "Shadcn UI",
-        "Magic UI",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://chatcollect.com",
-          icon: <Icons.globe className="size-3" />,
-        },
-      ],
-      image: "",
-      video:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
-    },
-    {
-      title: "Magic UI",
-      href: "https://magicui.design",
-      dates: "June 2023 - Present",
-      active: true,
-      description:
-        "Designed, developed and sold animated UI components for developers.",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Stripe",
-        "Shadcn UI",
-        "Magic UI",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://magicui.design",
-          icon: <Icons.globe className="size-3" />,
-        },
-        {
-          type: "Source",
-          href: "https://github.com/magicuidesign/magicui",
-          icon: <Icons.github className="size-3" />,
-        },
-      ],
-      image: "",
-      video: "https://cdn.magicui.design/bento-grid.mp4",
-    },
-    {
-      title: "llm.report",
-      href: "https://llm.report",
-      dates: "April 2023 - September 2023",
-      active: true,
-      description:
-        "Developed an open-source logging and analytics platform for OpenAI: Log your ChatGPT API requests, analyze costs, and improve your prompts.",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Shadcn UI",
-        "Magic UI",
-        "Stripe",
-        "Cloudflare Workers",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://llm.report",
-          icon: <Icons.globe className="size-3" />,
-        },
-        {
-          type: "Source",
-          href: "https://github.com/dillionverma/llm.report",
-          icon: <Icons.github className="size-3" />,
-        },
-      ],
-      image: "",
-      video: "https://cdn.llm.report/openai-demo.mp4",
-    },
-    {
-      title: "Automatic Chat",
-      href: "https://automatic.chat",
-      dates: "April 2023 - March 2024",
-      active: true,
-      description:
-        "Developed an AI Customer Support Chatbot which automatically responds to customer support tickets using the latest GPT models.",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Shadcn UI",
-        "Magic UI",
-        "Stripe",
-        "Cloudflare Workers",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://automatic.chat",
-          icon: <Icons.globe className="size-3" />,
-        },
-      ],
-      image: "",
-      video:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/automatic-chat.mp4",
-    },
-  ],
+  // projects: [
+  //   {
+  //     title: "Chat Collect",
+  //     href: "https://chatcollect.com",
+  //     dates: "Jan 2024 - Feb 2024",
+  //     active: true,
+  //     description:
+  //       "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+  //     technologies: [
+  //       "Next.js",
+  //       "Typescript",
+  //       "PostgreSQL",
+  //       "Prisma",
+  //       "TailwindCSS",
+  //       "Stripe",
+  //       "Shadcn UI",
+  //       "Magic UI",
+  //     ],
+  //     links: [
+  //       {
+  //         type: "Website",
+  //         href: "https://chatcollect.com",
+  //         icon: <Icons.globe className="size-3" />,
+  //       },
+  //     ],
+  //     image: "",
+  //     video:
+  //       "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+  //   },
+  //   {
+  //     title: "Magic UI",
+  //     href: "https://magicui.design",
+  //     dates: "June 2023 - Present",
+  //     active: true,
+  //     description:
+  //       "Designed, developed and sold animated UI components for developers.",
+  //     technologies: [
+  //       "Next.js",
+  //       "Typescript",
+  //       "PostgreSQL",
+  //       "Prisma",
+  //       "TailwindCSS",
+  //       "Stripe",
+  //       "Shadcn UI",
+  //       "Magic UI",
+  //     ],
+  //     links: [
+  //       {
+  //         type: "Website",
+  //         href: "https://magicui.design",
+  //         icon: <Icons.globe className="size-3" />,
+  //       },
+  //       {
+  //         type: "Source",
+  //         href: "https://github.com/magicuidesign/magicui",
+  //         icon: <Icons.github className="size-3" />,
+  //       },
+  //     ],
+  //     image: "",
+  //     video: "https://cdn.magicui.design/bento-grid.mp4",
+  //   },
+  //   {
+  //     title: "llm.report",
+  //     href: "https://llm.report",
+  //     dates: "April 2023 - September 2023",
+  //     active: true,
+  //     description:
+  //       "Developed an open-source logging and analytics platform for OpenAI: Log your ChatGPT API requests, analyze costs, and improve your prompts.",
+  //     technologies: [
+  //       "Next.js",
+  //       "Typescript",
+  //       "PostgreSQL",
+  //       "Prisma",
+  //       "TailwindCSS",
+  //       "Shadcn UI",
+  //       "Magic UI",
+  //       "Stripe",
+  //       "Cloudflare Workers",
+  //     ],
+  //     links: [
+  //       {
+  //         type: "Website",
+  //         href: "https://llm.report",
+  //         icon: <Icons.globe className="size-3" />,
+  //       },
+  //       {
+  //         type: "Source",
+  //         href: "https://github.com/dillionverma/llm.report",
+  //         icon: <Icons.github className="size-3" />,
+  //       },
+  //     ],
+  //     image: "",
+  //     video: "https://cdn.llm.report/openai-demo.mp4",
+  //   },
+  //   {
+  //     title: "Automatic Chat",
+  //     href: "https://automatic.chat",
+  //     dates: "April 2023 - March 2024",
+  //     active: true,
+  //     description:
+  //       "Developed an AI Customer Support Chatbot which automatically responds to customer support tickets using the latest GPT models.",
+  //     technologies: [
+  //       "Next.js",
+  //       "Typescript",
+  //       "PostgreSQL",
+  //       "Prisma",
+  //       "TailwindCSS",
+  //       "Shadcn UI",
+  //       "Magic UI",
+  //       "Stripe",
+  //       "Cloudflare Workers",
+  //     ],
+  //     links: [
+  //       {
+  //         type: "Website",
+  //         href: "https://automatic.chat",
+  //         icon: <Icons.globe className="size-3" />,
+  //       },
+  //     ],
+  //     image: "",
+  //     video:
+  //       "https://pub-83c5db439b40468498f97946200806f7.r2.dev/automatic-chat.mp4",
+  //   },
+  // ],
   hackathons: [
     {
       title: "PSEG Intern Website",
@@ -232,17 +232,36 @@ export const DATA = {
         {
           title: "Source",
           icon: <Icons.github className="h-4 w-4" />,
-          href: "hel",
+          href: "",
         },
         {
-          tools: ["React", "CSS"]
+          tools: ["React", "CSS", "Java", "JavaScript"]
         }
       ],
     },
     {
-      title: "Portal Hackathon",
+      title: "Android Wellness App",
+      dates: "October, 2024",
+      location: "",
+      description:
+          "I created a knowledge base to streamline intern onboarding, organized status meetings for team coordination, " +
+          "used GitHub for version control, and collaborated with the content team on a user-friendly intern guide.",
+      image:"/pseg.png",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "",
+        },
+        {
+          tools: ["React", "CSS", "Java", "JavaScript"]
+        }
+      ],
+    },
+    {
+      title: "Weather Web App",
       dates: "October 29, 2016",
-      location: "Kingston, Ontario",
+      location: "",
       description:
         "Developed an internal widget for uploading assignments using Waterloo's portal app",
       image:
@@ -251,8 +270,49 @@ export const DATA = {
         {
           title: "Source",
           icon: <Icons.github className="h-4 w-4" />,
-          href: "https://github.com/UWPortalSDK/crowmark",
+          href: "https://github.com/Andy-Not/betterWeather",
         },
+        {
+          tools: ["React", "CSS", "Java", "JavaScript", "RestAPI"]
+        }
+      ],
+    },
+    {
+      title: "Project Management Web App",
+      dates: "October 29, 2016",
+      location: "",
+      description:
+          "Developed an internal widget for uploading assignments using Waterloo's portal app",
+      image:
+          "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/portal-hackathon.png",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/Andy-Not/betterWeather",
+        },
+        {
+          tools: ["React", "CSS", "Java", "JavaScript", "RestAPI"]
+        }
+      ],
+    },
+    {
+      title: "Messaging App",
+      dates: "October, 2024",
+      location: "",
+      description:
+          "I created a knowledge base to streamline intern onboarding, organized status meetings for team coordination, " +
+          "used GitHub for version control, and collaborated with the content team on a user-friendly intern guide.",
+      image:"/pseg.png",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "",
+        },
+        {
+          tools: ["React", "CSS", "Java", "JavaScript"]
+        }
       ],
     },
   ],
